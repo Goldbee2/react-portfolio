b062337229c857e51fccfd91656312280a539a4d
Eli Goldberg <goldbee2@wwu.edu>
2021-10-04 14:51:31 -0700

    Created dynamic statbar component

---
+++ src/Components/StatBar.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import "./StatBar.css"
+
+
+function StatBar({bar_width, color1, color2, label}) {
+
+    const outerStyles = {
+        width: "120px",
+        height: "12px",
+        backgroundColor: "whitesmoke",
+        boxShadow: "inset 0px 0px 4px -2px rgba(0, 0, 0, 0.3)",
+        borderRadius: "20px"
+    }
+
+    const innerStyles = {
+        background: `linear-gradient(to right, ${color1} 0%, ${color2} 100%)`,
+        width: `${bar_width}%`,
+        height: "100%",
+        borderRadius: "inherit",
+        textAlign: "right"
+    }
+
+    const containerStyles = {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        margin: "0.4em 1em",
+        alignItems: "center",
+        padding: "0 0.8em"
+    }
+
+    const labelStyles = {
+        fontSize: 12,
+    }
+
+    return (
+        <div style={containerStyles}>
+            <label style={labelStyles}>{label}</label>
+            <div style={outerStyles}>
+                <div style={innerStyles}/>
+            </div>    
+        </div>
+    )}
+
+
+export default StatBar
